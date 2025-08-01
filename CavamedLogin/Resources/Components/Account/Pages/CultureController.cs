451196946288ec2f06bfe3cd444fc9241bea937b
dastaneray1234 <dastaneray123@hotmail.com>
2025-08-01 15:57:24 +0300

    geliştirme

---
+++ CavamedLogin/Resources/Components/Account/Pages/CultureController.cs
@@ -0,0 +1,23 @@
+﻿// Controllers/CultureController.cs
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]/[action]")]
+public class CultureController : Controller
+{
+    // Controllers/CultureController.cs
+    public IActionResult Set(string culture, string redirectUri)
+    {
+        if (culture != null)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                // BU SATIRI EKLEYEREK COOKIE'Yİ 1 YIL BOYUNCA GEÇERLİ KILIYORUZ
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+        }
+
+        return LocalRedirect(redirectUri);
+    }
+}
